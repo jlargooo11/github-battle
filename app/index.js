5ca845327f13f562e9b4485e010ba074b1e85b06
Jun Largo <rklargsjr@gmail.com>
2020-04-21 14:22:55 +0800

    set up app folder

---
+++ app/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+
+
+class App extends React.Component {
+    render() {
+        return(
+            <h2>
+                Hello World
+            </h2>
+        )
+    }
+}
+
+
+ReactDOM.render(
+    < App />,
+    document.getElementById('app')
+)
